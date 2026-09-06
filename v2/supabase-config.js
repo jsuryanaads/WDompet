c7d7d2c48ef515f1618642ea93227491b7ed0a58
jsuryanaads <jsuryanaads@gmail.com>
2026-09-06 21:48:35 +0700

    fix(v2): set Supabase publishable configuration

--- v2/supabase-config.js
+++ v2/supabase-config.js
@@ -1,1 +1,1 @@
-window.WDOMPET_SUPABASE={url:'https://ipclsfyabjgfeqpiqtlo.supabase.co',key:'https://ipclsfyabjgfeqpiqtlo.supabase.co/rest/v1/'};
+window.WDOMPET_SUPABASE={url:'https://ipclsfyabjgfeqpiqtlo.supabase.co',key:'sb_publishable_TvM7TcAPqVtvKes5dBk8Xg_PTe3bYzs'};
